c82c624273af905d1184ca6941c53a7153c5cff7
NguyenVanAnh98 <nguyenanhtk321@gmail.com>
2024-05-30 10:01:40 +0700

    first commit

---
+++ src/main/java/com/example/md4_bai14springsecurity/security/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.example.md4_bai14springsecurity.security;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
